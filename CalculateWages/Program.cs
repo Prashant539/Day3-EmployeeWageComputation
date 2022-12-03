81989ab1b220d5c80a9e8a45a4441056f7ca1e99
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-03 20:03:23 +0530

    Daily Employee Wage

---
+++ CalculateWages/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CalculateWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WagesTillCondition wagesTillCondition = new WagesTillCondition();
+            wagesTillCondition.CheckCondition();
+
+        }
+    }
+}

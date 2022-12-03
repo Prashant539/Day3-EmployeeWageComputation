f83863c6c62921a3aa66ccaea40324586aca384c
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-03 20:19:04 +0530

    Switch Case Statement

---
+++ EmployeeWageSwitchCase/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace EmployeeWageSwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SwitchCase switchCase = new SwitchCase();
+            switchCase.CheckCases();
+
+        }
+    }
+}
+

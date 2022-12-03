ef4928912d42d7155101c4aff76ddedf0df82dbf
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-03 20:14:10 +0530

    Part Time Employee and Wage

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartTime partTime = new PartTime();
+            partTime.CheckPartTime();
+
+        }
+    }
+}

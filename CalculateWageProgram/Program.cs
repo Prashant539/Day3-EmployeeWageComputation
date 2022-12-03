209334b5d49dc6b91220ee6a652569dc84cdff4d
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-03 20:36:45 +0530

    Calculate Wages Till Conditions

---
+++ CalculateWageProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CalculateWageProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WagesCondition wagesCondition = new WagesCondition();
+            wagesCondition.CheckCondition();
+
+        }
+    }
+}

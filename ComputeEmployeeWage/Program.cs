8958945964d7c94b06eaa01dfa7d42c9357f46e3
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-06 18:02:24 +0530

    Write a class method to compute employee wage

---
+++ ComputeEmployeeWage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ComputeEmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputeWages computeWages = new ComputeWages();
+            computeWages.CheckCondition();
+
+        }
+    }
+}
